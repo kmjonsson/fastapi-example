e4f95fe49d28050adf7edfd14af677e005a154eb
Magnus Jonsson <fot@fot.nu>
2025-11-01 15:17:33 +0100

    Cleanup

--- fastapi_backend/events/github/router.py
+++ fastapi_backend/events/github/router.py
@@ -6,6 +6,7 @@ from fastapi_backend.log import log
 
 api = APIRouter(prefix="/events/github")
 
+
 @api.post("/")
 async def github_event_endpoint(event: GithubWebHook) -> GithubWebHook:
     log.info(f"Received GitHub event: {event.hook.events}")
--- test/test_github.py
+++ test/test_github.py
@@ -2,19 +2,14 @@
 
 from fastapi.testclient import TestClient
 
-from fastapi_backend.database import db, Base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
-import pytest
-
 from fastapi_backend.main import app
 
 client = TestClient(app)
 
+
 def test_event():
     response = client.post(
         "/events/github",
-        json={"hook_id": 578418866,"hook": {"id": 578418866,"events": ["push" ]}}
+        json={"hook_id": 578418866, "hook": {"id": 578418866, "events": ["push"]}},
     )
     assert response.status_code == 200
